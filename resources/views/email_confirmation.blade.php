ca167f1f965d8d117ea7f9fb3446bc925c19744d
Haider Ali Salhi <salhiali197@gmail.com>
2022-12-18 07:31:11 +0100

    Fix EMail

--- resources/views/email_confirmation.blade.php
+++ resources/views/email_confirmation.blade.php
@@ -8,12 +8,10 @@ Confirmation de votre demande evisa ALgérie<br>
 Conseils de sécurité :<br>
 
 * Ne donnez jamais votre mot de passe à qui que ce soit.<br>
-* N'appelez jamais le numéro de téléphone d'une personne prétendant être l'assistance GALAXY.<br>
-* N'envoyez jamais d'argent à quiconque prétendant être membre de l'équipe Galaxy.<br>
-* Ajoutez https://app.mygalaxy.world à vos favoris et utilisez https://www.mygalaxy.world notre site officiel pour toute information. <br>
-Si vous ne reconnaissez pas cette activité, veuillez contacter immédiatement notre service client à l'adresse : https://app.mygalaxy.world/support . 
+* N'appelez jamais le numéro de téléphone d'une personne prétendant être l'assistance .<br>
+* N'envoyez jamais d'argent à quiconque prétendant être membre de l'équipe .<br>
 <br>
-Équipe Galaxy <br>
+Équipe Evisa <br>
 Ceci est un message automatisé, veuillez ne pas répondre.
 
 
